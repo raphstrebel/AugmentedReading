1615390e48a4d4a0140b109aa02a170a8a1a3062
raphstrebel <raphael.strebel@epfl.ch>
2019-11-17 00:10:56 +0100

    handle emotions

---
+++ emotions.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def get_sound_from_keywords(keywords):
+
+    word_emotion = pd.read_csv("emotion_db.csv")
+    word_emotion = word_emotion.drop(columns=["anticipation", "surprise", "trust"])
+
+    keywords_df = pd.DataFrame(keywords, columns= ["word"], dtype="str")
+    keywords_emotion = word_emotion.merge(keywords_df, on="word").drop(columns="word")
+    
+    return keywords_emotion.sum(axis=0).to_dict()
+    
