d9cc41d532ae8db2c2a9bb6237113b45936dcd7f
Gilbert Maystre <gilbert@maystre.ch>
2019-11-16 23:31:30 +0100

    improv.

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ import time
 
 FOCUS_VALUE = 15
 WINDOW_SIZE = 55
+font = cv2.FONT_HERSHEY_SIMPLEX
+white = (255, 255, 255)
 
-def display_frame(frame, text, color):
+def to_BGR(color):
+	(r, g, b) = color
+	hey =  (int(b * 255), int(g * 255), int(r * 255))
+	print(hey)
+	return hey
 
+def display_frame(frame, text, color, iteration):
 	### hack
 	cv2.imwrite('todel.png', frame)
 
@@ -24,49 +31,40 @@ def display_frame(frame, text, color):
 	### Oufff, we're good to go :-)
 	yo2 = cv2.copyMakeBorder(yo2, 0, 0, 0, 800, cv2.BORDER_CONSTANT)
 
-	cv2.rectangle(yo, (405, 0), (430, 720), [int(i*255) for i in color], -1)
+	### print color rectangle
+	cv2.rectangle(yo2, (405, 0), (430, 720), to_BGR(color), -1)
 
-
-	font                   = cv2.FONT_HERSHEY_SIMPLEX 	
-	fontScale              = 1
-	fontColor              = (255, 255, 255)
-	lineType               = 2
-
+	### print keywords occurences
+	occurrences = get_occurrences_from_text(text)
 	for i in range(len(text)):
 		bottomLeftCornerOfText = (430, i * 40 + 20)
-		cv2.putText(yo2, text[i], bottomLeftCornerOfText, font, fontScale,fontColor,lineType)
+		cv2.putText(yo2, text[i], bottomLeftCornerOfText, font, 1, white, 2)
 
-	cv2.imshow("Capturing", yo2)
+	### print iteration number
+	cv2.putText(yo2, "iteration: " + str(iteration), (700, 80), font, 2, white, 2)
 
-def get_occurrences_from_keywords(keywords):
-    occurrences = [(word, keywords.count(word)) for word in set(keywords)]
-    sorted_occurrences_str = [ word +' '+ str(occ) for word, occ in sorted(occurrences, key=lambda x:-x[1])]
-    return sorted_occurrences_str
+	cv2.imshow("Control panel", yo2)
+
+def get_occurrences_from_text(text):
+	keywords = get_keywords_from_text(text)
+	occurrences = [(word, keywords.count(word)) for word in set(keywords)]
+	sorted_occurrences_str = [ word +' '+ str(occ) for word, occ in sorted(occurrences, key=lambda x:-x[1])]
+	return sorted_occurrences_str
 
 if __name__=="__main__":
-
     webcam = setup_webcam(FOCUS_VALUE)
-
-
+    iteration = 1
     while(True):
         capture_frame(webcam, WINDOW_SIZE) # To remove the last frame from the buffer
         
         #### MAIN BODY OF THE LOOP
         frame = capture_frame(webcam, WINDOW_SIZE)
-        text = " ".join(extract_text(frame))
-        #color = get_color_for_words(text)
-        #text = "him—“we are now close upon the Norwegian coast—in the sixty-eighth degree of latitude— in the great province of Nordland—and in the dreary district of Lofoden.  e mountain upon whose top we sit is Helseggen, the Cloudy. Now raise yourself up a little higher—hold on to the grass if you feel giddy—so—and look out, beyond the belt of vapor beneath us, into the sea.” I looked dizzily, and beheld a wide expanse of ocean, whose waters wore so inky a hue as to bring at once to my mind the Nubian geographer’s account of the Mare Tenebrarum. A panorama more deplorably desolate no hu- man imagination can conceive. To the right and left, as far as the eye could reach, there lay outstretched, like ramparts of the world, lines of horridly black and beetling cli , whose character of gloom was but the more forcibly illustrated by the surf which reared high up against its white and ghastly crest, howling and shrieking forever. Just opposite the promon- tory upon whose apex we were placed, and at a distance of some  ve or six miles out at sea, there was visible a small, bleak-looking island; or, more properly, its position was discern- ible through the wilderness of surge in which it was enveloped. About two miles nearer the land, arose another of smaller size, hideously craggy and barren, and encompassed at vari- ous intervals by a cluster of dark rocks.  e appearance of the ocean, in the space between the more distant island and the shore, had something very unusual about it. Although, at the time, so strong a gale was blowing land- ward that a brig in the remote o ng lay to"
-        r,g,b, brightness = text_to_colors_and_emotions(text)
-        keywords = get_keywords_from_text(text)
-        occurrences = get_occurrences_from_keywords(keywords)
-        print(r,g,b,brightness)
-        displayNewColor(r,g,b,brightness=brightness)
-        display_frame(frame, occurrences, color=(r,b,g))
-
+        text = extract_text(frame)
+        (r, g, b, brightness) = text_to_colors_and_emotions(text)
+        displayNewColor(r, g, b, brightness=brightness)
+        display_frame(frame, text, (r,g,b), iteration)
         #### END MAIN BODY
 
         key = cv2.waitKey(1)
         time.sleep(2)
-
-
-    
+        iteration+=1
--- text_extraction.py
+++ text_extraction.py
@@ -7,18 +7,7 @@ import pytesseract
 # '--oem 1' for using LSTM OCR Engine
 config = ('-l eng --oem 1 --psm 3')
 
-def sanitize_text(text):
-  toReturn = text.split("\n")
-  return toReturn
-
-
 def extract_text(frame):
   extracted = pytesseract.image_to_string(frame, config=config)
-
-  lines = extracted.split("\n")
-
-  lines = list(filter(lambda a: len(a) > 0, lines))
-
-
-
-  return lines
+  extracted = extracted.replace("\n", "")
+  return extracted
