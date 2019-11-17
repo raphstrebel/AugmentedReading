ce311ba5939a125a2c014b72f89513d0e29ee306
Emilio <emilefontanet@gmail.com>
2019-11-17 03:13:46 +0100

    music update

--- play_music.py
+++ play_music.py
@@ -5,22 +5,43 @@ from pygame import mixer
 pygame.init()
 
 current_song = ""
+current_music = ""
+sound_channel = 0
+def play_music(music_name):
+	global current_music
+	if(current_music == music_name):
+		return
+	else:
+		try:
+			music_path = 'sound/' + music_name + '.wav'
+			mixer.music.load(music_path)
+			mixer.music.fadeout(2000)
+			mixer.music.play(-1)
+			current_music = music_path
+		except:
+			pass
 
 def play_sound(song_name):
 	global current_song
+	global sound_channel
 	if(current_song == song_name):
 		return
 	else:
 		try:
 			song_path = 'sound/' + song_name + '.wav'
-			mixer.music.load(song_path)
-			mixer.music.fadeout(1500)
-			mixer.music.play(-1)
+			sound = mixer.Sound(song_path)
+			mixer.Channel(sound_channel).fadeout(2000)
+			mixer.Channel((sound_channel + 1)%2).play(sound,-1, fade_ms = 2000)
+			#mixer.music.load(song_path)
+			#mixer.music.fadeout(1500)
+			#mixer.music.play(-1)
 			current_song = song_path
+			sound_channel = (sound_channel + 1) % 2
 		except:
 			pass
 
 def stop_sound():
 	global current_song
 	current_song = ""
-	mixer.music.stop()
+	mixer.Channel(0).stop()
+	mixer.Channel(1).stop()
