a6f95a63fa817ec6f50c40656e23d2e771ef2201
Gilbert Maystre <gilbert@maystre.ch>
2019-11-17 02:36:12 +0100

    fuuuu

--- play_music.py
+++ play_music.py
@@ -5,6 +5,7 @@ from pygame import mixer
 pygame.init()
 
 current_song = ""
+<<<<<<< Updated upstream
 
 def play_sound(song_name):
 	global current_song
@@ -25,3 +26,20 @@ def stop_sound():
 	current_song = ""
 	mixer.music.stop()
 
+=======
+song_to_path = {
+	'dragon' : 'sound/dragon_shout.mp3',
+	'rain' : 'sound/light_rain.mp3',
+	'storm' : 'sound/rainy_storm.mp3'
+}
+
+def play_sound(song_path):
+    global current_song
+    if(current_song == song_path):
+        return
+    else:
+        mixer.music.fadeout(1500)
+        mixer.music.load(song_path)
+        mixer.music.play(-1)
+        current_song = song_path
+>>>>>>> Stashed changes
