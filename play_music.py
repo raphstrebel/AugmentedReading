c875c714d249f21d3d12d4448f257fa7f49ba758
Emilio <emilefontanet@gmail.com>
2019-11-17 02:29:53 +0100

    play_music.py update

--- play_music.py
+++ play_music.py
@@ -5,17 +5,23 @@ from pygame import mixer
 pygame.init()
 
 current_song = ""
-song_to_path = {
-	'dragon' : 'sound/dragon_shout.mp3',
-	'rain' : 'sound/light_rain.mp3',
-	'storm' : 'sound/rainy_storm.mp3'
-}
-def play_sound(song_path):
-    global current_song
-    if(current_song == song_path):
-        return
-    else:
-        mixer.music.fadeout(1500)
-        mixer.music.load(song_path)
-        mixer.music.play(-1)
-        current_song = song_path
+
+def play_sound(song_name):
+	global current_song
+	if(current_song == song_name):
+		return
+	else:
+		try:
+			song_path = 'sound/' + song_name + '.wav'
+			mixer.music.load(song_path)
+			mixer.music.fadeout(1500)
+			mixer.music.play(-1)
+			current_song = song_path
+		except:
+			pass
+
+def stop_sound():
+	global current_song
+	current_song = ""
+	mixer.music.stop()
+
